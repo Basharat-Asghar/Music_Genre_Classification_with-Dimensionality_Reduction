a363142c206e1b77cc15edae5fc7a893a383edc7
Basharat-Asghar <mbasharatasghar1144@gmail.com>
2026-01-23 09:59:14 +0500

    implement predict pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -43,3 +43,52 @@ class PredictPipeline:
 
         except Exception as e:
             raise CustomException(e, sys)
+        
+class CustomData:
+    def __init__(self,
+                 tempo: float,
+                 dynamics_range: float,
+                 vocal_presence: float,
+                 percussion_strength: float,
+                 string_instrument_detection: float,
+                 electronic_element_presence: float,
+                 rhythm_complexity: float,
+                 drums_influence: float,
+                 distorted_guitar: float,
+                 metal_frequencies: float,
+                 ambient_sound_influence: float,
+                 instrumental_overlaps: float):
+        self.tempo = tempo
+        self.dynamics_range = dynamics_range
+        self.vocal_presence = vocal_presence
+        self.percussion_strength = percussion_strength
+        self.string_instrument_detection = string_instrument_detection
+        self.electronic_element_presence = electronic_element_presence
+        self.rhythm_complexity = rhythm_complexity
+        self.drums_influence = drums_influence
+        self.distorted_guitar = distorted_guitar
+        self.metal_frequencies = metal_frequencies
+        self.ambient_sound_influence = ambient_sound_influence
+        self.instrumental_overlaps = instrumental_overlaps
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "tempo": [self.tempo],
+                "dynamics_range": [self.dynamics_range],
+                "vocal_presence": [self.vocal_presence],
+                "percussion_strength": [self.percussion_strength],
+                "string_instrument_detection": [self.string_instrument_detection],
+                "electronic_element_presence": [self.electronic_element_presence],
+                "rhythm_complexity": [self.rhythm_complexity],
+                "drums_influence": [self.drums_influence],
+                "distorted_guitar": [self.distorted_guitar],
+                "metal_frequencies": [self.metal_frequencies],
+                "ambient_sound_influence": [self.ambient_sound_influence],
+                "instrumental_overlaps": [self.instrumental_overlaps]
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+        
+        except Exception as e:
+            raise CustomException(e, sys)
